0113e042d6318a89e8a2183796b0450561cb6a69
Dmitry <dima.zer12@gmail.com>
2020-04-16 03:39:17 +0300

    Первый комит

---
+++ main.js
@@ -0,0 +1,79 @@
+// *****1 task****
+// alert('Я JavaScript');
+
+// *****2task***
+// let name = "Джон", admin = name;
+// alert(admin);
+
+// ******Task 3 ********
+
+// let earthPlanet = "Планета",
+// 	userName = 20;
+
+
+// **********TASK 4************
+// let name = "Ilya";
+// alert (`hello ${1}`);
+// alert (`hello ${"name"}`);
+// alert (`hello ${name}`);
+
+// ***********TASK 5 ***********
+// let age = prompt ("Сколько вам лет?");
+// alert(age);
+
+// *************TASK 6***********
+//  let age = prompt('Сколько вам лет?');
+//  if (age > 18){
+// 	 alert ("welcome");
+//  } else if(age < 18) {
+// 	 alert("Exit");
+//  }
+// *******TASK 6.1**********
+// let style= prompt('Сколько вам лет?'), age;
+//  if (style > 18){
+// 	 age = true;
+//  } else  {
+// 	 age = false;
+//  }
+//  alert(age );
+
+// *********TASK 6.2***********
+// let persone = prompt('Ведите логин'), login;
+// (persone == "Сотрудник") ? login = "Привет Сотрудник":
+// (persone == "Директор") ? login = "Здраствуйте босс":
+// (persone == "") ? login = "Нет логина":
+// "";
+// alert(login);   
+
+
+// ***************Task 7  if ****************
+// let browser = prompt("Browser");
+// if(browser == "Edge"){
+// 	alert("You've  got the Edge!");
+// } else if (browser == "Chrome"
+// || browser == "Firefox"
+// || browser == "Safari"
+// || browser == "Opera"){
+// 	alert("Okay we support these browsers too");
+// }else {
+// 	alert("We hope that this page looks ok!");
+// }
+
+
+// ***************Task 7  SWITCH ****************
+//  let persone = prompt("Ведите браузер"),browser;
+// switch (persone)  {
+// 	 case 'Edge':
+// 		 alert("you the Edge!");
+// 		 break;
+
+// 		 case "Chrome":
+// 		case "Firefox":
+// 		case   "Safari":
+// 			case "Opera":
+// 				alert("OK we support");
+// 				break;
+				
+// 				default:
+// 					alert("looks ok!");
+//  }
